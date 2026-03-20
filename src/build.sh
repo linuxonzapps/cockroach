0267a7ce811b977b56c1fea36807c935a207e507
rposts <rishi.investigate@gmail.com>
2026-03-20 19:33:17 +0000

    Clone script repo
    Signed-off-by: rposts <rishi.investigate@gmail.com>

--- src/build.sh
+++ src/build.sh
@@ -13,6 +13,9 @@ if [ -f /etc/os-release ]; then
 fi
 current_dir="$PWD"
 echo $DISTRO > .distro_zab.txt
+sudo apt update; sudo apt install git -y
+# Clone linux-on-ibm-z to keep it current
+git clone https://github.com/linux-on-ibm-z/scripts.git /tmp/linux-on-ibm-z
 bash /tmp/linux-on-ibm-z-scripts/CockroachDB/${version}/build_crdb.sh -y
 tar cvfz cockroach-${version}-linux-s390x.tar.gz -C /usr/local/bin cockroach -C /usr/local/lib cockroach
 exit 0
