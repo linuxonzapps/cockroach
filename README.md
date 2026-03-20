9b7603a912e40260869a62aff96acd71eedb70ac
rposts <rishi.investigate@gmail.com>
2026-03-20 17:09:32 +0000

    Add CockroachDB processing
    Signed-off-by: rposts <rishi.investigate@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# CockroachDB
+
+CockroachDB is a cloud-native distributed SQL database designed to build,
+scale, and manage modern, data-intensive applications.
+
+Additional information available [here](https://github.com/cockroachdb/cockroach).
+
+>**Note:** Artifacts available in this repository are targeted for `s390x` architecture.
---
+++ src/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -o pipefail
+read -ra arr <<< "$@"
+version=${arr[1]}
+trap 0 1 2 ERR
+# Ensure sudo is installed
+apt-get update && apt-get install sudo -y
+# Extract DISTRO details for tagging
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO="$ID-$VERSION_ID"
+    if [ "$VERSION_CODENAME" != "" ]; then
+        DISTRO="$ID-$VERSION_CODENAME"
+    fi
+fi
+current_dir="$PWD"
+echo $DISTRO > .distro_zab.txt
+bash /tmp/linux-on-ibm-z-scripts/CockroachDB/${version}/build_crdb.sh -y
+tar cvfz cockroach-${version}-linux-s390x.tar.gz -C /usr/local/bin cockroach -C /usr/local/lib cockroach
+exit 0
